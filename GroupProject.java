07f29a753f87cf0e996667f6b9e1b510393bafb8
ethanzhang21 <ethan.m.zhang@gmail.com>
2025-04-04 09:23:10 -0700

    guiii

--- GroupProject.java
+++ GroupProject.java
@@ -2,13 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GroupProject {
 
+public class GroupProject {
     private JFrame frame;
     private JButton[][] buttons = new JButton[8][8];
     private Piece[][] board = new Piece[8][8];
     private int selectedRow = -1;
     private int selectedCol = -1;
+    private boolean isWhiteTurn = true;
+
 
     public GroupProject() {
         frame = new JFrame("Chess");
@@ -20,6 +22,7 @@ public class GroupProject {
         frame.setVisible(true);
     }
 
+
     private void setupBoard() {
         // Pawns
         for (int col = 0; col < 8; col++) {
@@ -27,11 +30,13 @@ public class GroupProject {
             board[6][col] = new Pawn(6, col, true);
         }
 
+
         // Rooks
         board[0][0] = new Rook(0, 0, false);
         board[0][7] = new Rook(0, 7, false);
         board[7][0] = new Rook(7, 0, true);
         board[7][7] = new Rook(7, 7, true);
+
 
         // Knights
         board[0][1] = new Knight(0, 1, false);
@@ -39,20 +44,24 @@ public class GroupProject {
         board[7][1] = new Knight(7, 1, true);
         board[7][6] = new Knight(7, 6, true);
 
+
         // Bishops
         board[0][2] = new Bishop(0, 2, false);
         board[0][5] = new Bishop(0, 5, false);
         board[7][2] = new Bishop(7, 2, true);
         board[7][5] = new Bishop(7, 5, true);
 
+
         // Queens
         board[0][3] = new Queen(0, 3, false);
         board[7][3] = new Queen(7, 3, true);
+
 
         // Kings
         board[0][4] = new King(0, 4, false);
         board[7][4] = new King(7, 4, true);
     }
+
 
     private void setupButtons() {
         for (int r = 0; r < 8; r++) {
@@ -62,7 +71,7 @@ public class GroupProject {
                 button.setFont(new Font("Arial", Font.PLAIN, 48));
                 Color color;
                 if ((r + c) % 2 == 0)
-                    color = new Color(240, 217, 181);
+                    color = new Color(241, 191, 191);
                 else
                     color = new Color(181, 136, 99);
                 button.setBackground(color);
@@ -72,10 +81,14 @@ public class GroupProject {
         }
     }
 
+
     private String getPieceSymbol(Piece p) {
-        if (p == null) return "";
+        if (p == null){
+            return "";
+        }
         return p.symbol();
     }
+
 
     public void handleClick(int row, int col) {
         if (selectedRow == -1 && board[row][col] != null) {
@@ -83,16 +96,18 @@ public class GroupProject {
             selectedCol = col;
         } else if (selectedRow != -1) {
             Piece selected = board[selectedRow][selectedCol];
-            if (selected.canMove(row, col, board)) {
+            if (selected.canMove(row, col, board) && selected.isWhite == isWhiteTurn) {
                 board[row][col] = selected;
                 selected.move(row, col);
                 board[selectedRow][selectedCol] = null;
                 updateButtons();
+                isWhiteTurn = !isWhiteTurn;
             }
             selectedRow = -1;
             selectedCol = -1;
         }
     }
+
 
     private void updateButtons() {
         for (int r = 0; r < 8; r++)
@@ -100,15 +115,29 @@ public class GroupProject {
                 buttons[r][c].setText(getPieceSymbol(board[r][c]));
     }
 
+
     public static void main(String[] args) {
         new GroupProject();
     }
 }
+class Move {
+    int fromRow, fromCol, toRow, toCol;
+
+    public Move(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+}
+
+
 
 class CellButton extends JButton implements ActionListener {
     private int row;
     private int col;
     private GroupProject game;
+
 
     public CellButton(int row, int col, GroupProject game) {
         this.row = row;
@@ -116,6 +145,7 @@ class CellButton extends JButton implements ActionListener {
         this.game = game;
         addActionListener(this);
     }
+
 
     public void actionPerformed(ActionEvent e) {
         game.handleClick(row, col);
@@ -126,11 +156,13 @@ abstract class Piece {
     protected int x, y;
     protected boolean isWhite;
 
+
     public Piece(int x, int y, boolean isWhite) {
         this.x = x;
         this.y = y;
         this.isWhite = isWhite;
     }
+
 
     public abstract boolean canMove(int newX, int newY, Piece[][] board);
     public void move(int newX, int newY) {
@@ -138,35 +170,48 @@ abstract class Piece {
         y = newY;
     }
 
+
     public abstract String symbol();
 }
+
 
 // Pawn
 class Pawn extends Piece {
     private boolean hasMoved = false;
 
+
     public Pawn(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
+
 
     public boolean canMove(int newX, int newY, Piece[][] board) {
         int dir;
         if (isWhite) dir = -1;
         else dir = 1;
 
+
         if (!hasMoved && newX == x + 2 * dir && newY == y && board[newX][newY] == null)
             return true;
+
 
         if (newX == x + dir && newY == y && board[newX][newY] == null)
             return true;
 
+
+        if (newX == x + dir && Math.abs(newY - y) == 1 && board[newX][newY] != null && board[newX][newY].isWhite != this.isWhite)
+            return true;
+
+
         return false;
     }
+
 
     public void move(int newX, int newY) {
         super.move(newX, newY);
         hasMoved = true;
     }
+
 
     public String symbol() {
         if (isWhite) return "♙";
@@ -174,15 +219,18 @@ class Pawn extends Piece {
     }
 }
 
+
 // Rook
 class Rook extends Piece {
     public Rook(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
 
+
     public boolean canMove(int newX, int newY, Piece[][] board) {
         return newX == x || newY == y;
     }
+
 
     public String symbol() {
         if (isWhite) return "♖";
@@ -190,11 +238,13 @@ class Rook extends Piece {
     }
 }
 
+
 // Knight
 class Knight extends Piece {
     public Knight(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
+
 
     public boolean canMove(int newX, int newY, Piece[][] board) {
         int dx = Math.abs(newX - x);
@@ -202,11 +252,13 @@ class Knight extends Piece {
         return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
     }
 
+
     public String symbol() {
         if (isWhite) return "♘";
         else return "♞";
     }
 }
+
 
 // Bishop
 class Bishop extends Piece {
@@ -214,9 +266,11 @@ class Bishop extends Piece {
         super(x, y, isWhite);
     }
 
+
     public boolean canMove(int newX, int newY, Piece[][] board) {
         return Math.abs(newX - x) == Math.abs(newY - y);
     }
+
 
     public String symbol() {
         if (isWhite) return "♗";
@@ -224,15 +278,18 @@ class Bishop extends Piece {
     }
 }
 
+
 // Queen
 class Queen extends Piece {
     public Queen(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
 
+
     public boolean canMove(int newX, int newY, Piece[][] board) {
         return newX == x || newY == y || Math.abs(newX - x) == Math.abs(newY - y);
     }
+
 
     public String symbol() {
         if (isWhite) return "♕";
@@ -240,11 +297,13 @@ class Queen extends Piece {
     }
 }
 
+
 // King
 class King extends Piece {
     public King(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
+
 
     public boolean canMove(int newX, int newY, Piece[][] board) {
         int dx = Math.abs(newX - x);
@@ -252,8 +311,11 @@ class King extends Piece {
         return dx <= 1 && dy <= 1;
     }
 
+
     public String symbol() {
-        if (isWhite) return "♔";
+        if (isWhite){
+            return "♔";
+        }
         else return "♚";
     }
 }
